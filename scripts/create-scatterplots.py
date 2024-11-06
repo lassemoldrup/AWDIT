649d1f27153fbd51afe275f758a9e20ea9766458
Lasse Møldrup <lasse.moeldrup@gmail.com>
2024-11-06 19:39:25 +0100

    Improve scatterplots

--- scripts/create-scatterplots.py
+++ scripts/create-scatterplots.py
@@ -15,26 +15,32 @@ if __name__ == '__main__':
         with open(os.path.join(path, f'{date}-time-{isolation}.csv'), 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             if isolation == 'cc':
-                writer.writerow(['#txns', 'ours (s)', 'plume (s)', 'dbcop (s)', 'ours (res)', 'plume (res)', 'dbcop (res)'])
+                writer.writerow(['#txns', '#events', '#keys', 'ours (s)', 'plume (s)', 'dbcop (s)', 'ours (res)', 'plume (res)', 'dbcop (res)'])
             else:
-                writer.writerow(['#txns', 'ours (s)', 'plume (s)', 'ours (res)', 'plume (res)'])
+                writer.writerow(['#txns', '#events', '#keys', 'ours (s)', 'plume (s)', 'ours (res)', 'plume (res)'])
             
             times = {}
             results = {}
+            stats = {}
             for entry in os.listdir(path):
-                if not entry.endswith('.csv') or not f'-{isolation}-' in entry:
+                if not entry.endswith('.csv') or (not f'-{isolation}-' in entry and not entry.endswith('-stats.csv')):
                     continue
                 
-                if 'mem' in entry:
+                if entry.endswith('-mem.csv'):
                     continue
                 with open(os.path.join(path, entry), 'r') as csvfile:
                     reader = csv.reader(csvfile)
                     rows = list(reader)
-                    if 'time' in entry:
-                        times[entry.removesuffix('-time.csv')] = rows[1:]
-                    elif 'res' in entry:
+                    if entry.endswith('-time.csv'):
+                        times[entry.removesuffix('-time.csv')] = [row[1:] for row in rows[1:]]
+                    elif entry.endswith('-res.csv'):
                         results[entry.removesuffix('-res.csv')] = [row[1:] for row in rows[1:]]
+                    elif entry.endswith('-stats.csv'):
+                        elems = entry.split('-')[:-1]
+                        elems.insert(-1, isolation)
+                        stats['-'.join(elems)] = [row[1:] for row in rows[1:]]
             for k, ts in times.items():
                 res = results[k]
-                writer.writerows([t + r for t, r in zip(ts, res)])
+                sts = stats[k]
+                writer.writerows([s + t + r for s, t, r in zip(sts, ts, res)])
 
