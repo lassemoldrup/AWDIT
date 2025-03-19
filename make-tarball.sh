b95769990cf6e21694b738b66807604a86a48b9c
Lasse Møldrup <lasse.moeldrup@gmail.com>
2025-03-19 01:26:59 +0100

    Fix make-tarball

--- make-tarball.sh
+++ make-tarball.sh
@@ -13,5 +13,5 @@ cp Cargo.toml Cargo.lock build.rs README.md rustfmt.toml AWDIT
 docker buildx build --platform linux/amd64,linux/arm64 -f docker/Dockerfile -t awdit-artifact .
 docker save -o AWDIT/docker/awdit-artifact.tar.gz awdit-artifact
 
-chmod -R u+rwX,a+rX AWDIT
-tar -cJf AWDIT.tar.xz --uid 0 --gid 0 AWDIT
+chmod -R a+rwX AWDIT
+tar -cJf AWDIT.tar.xz --no-xattrs --uid 0 --gid 0 AWDIT
